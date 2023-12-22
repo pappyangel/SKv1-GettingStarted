8a188ecd21a16ecc1b3cd8b1bcbe83cca6cdf244
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2023-12-22 02:18:04 +0000

    setup test notebook

---
+++ Settings/AppConfig.cs
@@ -0,0 +1,52 @@
+// using System;
+// using System.Collections.Generic;
+// using System.IO;
+// using System.Text.Json;
+// using System.Threading.Tasks;
+// using Microsoft.DotNet.Interactive;
+// using InteractiveKernel = Microsoft.DotNet.Interactive.Kernel;
+// ReSharper disable InconsistentNaming
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.DotNet.Interactive;
+//using ConfigClass;
+
+public static class AppConfig
+{
+    private const string configFile = "../../secrets.json";
+    //private const string configFile = "../secrets.json";
+    
+
+    public class aoaiSettings
+    {        
+        public string? endpoint { get; set; } //= null;
+        public string? APIKey { get; set; } //= null;
+        public string? deployDavinci { get; set; } //= null;
+        public string? deployTurbo { get; set; } //= null;
+        public string? deployEmbed { get; set; } //= null;
+        public string? QdrantEndpoint { get; set; } //= null;
+        
+    }
+
+    public static aoaiSettings GetSecrets()
+    {        
+        aoaiSettings myConfig = new();
+
+        if (File.Exists(configFile))
+        {            
+            Console.WriteLine("file find");
+            myConfig = JsonSerializer.Deserialize<aoaiSettings>(File.ReadAllText(configFile));            
+        }
+        else
+        {
+            Console.WriteLine("file not find");
+        }
+
+        return myConfig;
+
+    }
+  
+}
